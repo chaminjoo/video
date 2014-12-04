29fb82a6f79191cfabcf5af098fa26c10ff519f1
chaminjoo <k22tmfrl@naver.com>
2014-12-04 16:32:55 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -9,10 +9,24 @@ function setup() {
                          ]);
   button = createButton('play');
   button.mousePressed(toggleVid); // attach button listener
+
+   fire = createVideo(['fire.mp4'
+                         ]);
+  button = createButton('play');
+  button.mousePressed(toggleVid); // attach button listener
 }
 
 // plays or pauses the video depending on current state
 function toggleVid() {
+  if (playing) {
+    fire.pause();
+    button.html('play');
+  } else {
+    fire.loop();
+    button.html('pause');
+  }
+  playing = !playing;
+
   if (playing) {
     fire.pause();
     button.html('play');
