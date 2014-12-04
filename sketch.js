b4b32869744266848bebd1f1cb7b9376c29288c2
chaminjoo <k22tmfrl@naver.com>
2014-12-04 14:57:58 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -1,26 +1,24 @@
+var playing = false;
 var fingers;
+var button;
+
 
 function setup() {
-  createCanvas(320, 240);
   // specify multiple formats for different browsers
   fingers = createVideo(['fire.mp4', 
                          'fire.webm']);
-  fingers.loop();
-  fingers.hide();
-  noStroke();
-  fill(0);
+  button = createButton('play');
+  button.mousePressed(toggleVid); // attach button listener
 }
 
-function draw() {
-  background(255);
-  fingers.loadPixels();
-  var stepSize = round(constrain(mouseX / 8, 6, 32));
-  for (var y=0; y<height; y+=stepSize) {
-    for (var x=0; x<width; x+=stepSize) {
-      var i = y * width + x;
-      var darkness = (255 - fingers.pixels[i*4]) / 255;
-      var radius = stepSize * darkness;
-      ellipse(x, y, radius, radius);
-    }
+// plays or pauses the video depending on current state
+function toggleVid() {
+  if (playing) {
+    fingers.pause();
+    button.html('play');
+  } else {
+    fingers.loop();
+    button.html('pause');
   }
+  playing = !playing;
 }
