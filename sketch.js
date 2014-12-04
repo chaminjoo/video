43815c580afc4f623a3d60eebf89cb6467f449d3
chaminjoo <k22tmfrl@naver.com>
2014-12-04 17:19:08 +0900

    dddd

--- sketch.js
+++ sketch.js
@@ -1,38 +1,21 @@
 var playing = false;
 var fire;
 var button;
-
+var night;
 
 function setup() {
+
+  createCanvas(1000,800);
   // specify multiple formats for different browsers
   fire = createVideo(['fire.mp4'
                          ]);
-  button = createButton('play');
-  button.mousePressed(toggleVid); // attach button listener
-
+  
    night = createVideo(['night.mp4'
                          ]);
-  button = createButton('play');
-  button.mousePressed(toggleVid); // attach button listener
+  
 }
 
-// plays or pauses the video depending on current state
-function toggleVid() {
-  if (playing) {
-    fire.pause();
-    button.html('play');
-  } else {
-    fire.loop();
-    button.html('pause');
-  }
-  playing = !playing;
-
-  if (playing) {
-    night.pause();
-    button.html('play');
-  } else {
-    fire.loop();
-    button.html('pause');
-  }
-  
+function draw(){
+  image(fire,0,0);
+  image(night,fire.width,0);
 }
